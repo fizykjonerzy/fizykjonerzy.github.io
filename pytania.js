ecaab099c168f013dda0038a8fcbcd91bb14e7c4
fizykjonerzy <MIKKOWA84@edu.lublin.eu>
2025-05-30 23:25:48 +0200

    beta

---
+++ pytania.js
@@ -0,0 +1,52 @@
+const pytania = [
+    {
+        pytanie: "Co jest jednostką natężenia prądu w układzie SI?",
+        odpowiedzi: ["Wolt", "Amper", "Om", "Kulomb"],
+        poprawna: 1
+      },
+      {
+        pytanie: "Jak nazywa się prawo opisujące zależność między napięciem, natężeniem prądu i oporem?",
+        odpowiedzi: ["Prawo Pascala", "Prawo Ohma", "Prawo Archimedesa", "Prawo Coulomba"],
+        poprawna: 1
+      },
+      {
+        pytanie: "Które urządzenie służy do pomiaru napięcia elektrycznego?",
+        odpowiedzi: ["Amperomierz", "Watomierz", "Woltomierz", "Ohmomierz"],
+        poprawna: 2
+      },
+      {
+        pytanie: "Jakie są nośniki prądu w metalach?",
+        odpowiedzi: ["Protony", "Elektrony", "Neutrony", "Jony dodatnie"],
+        poprawna: 1
+      },
+      {
+        pytanie: "Które z poniższych połączeń odbiorników powoduje, że prąd elektryczny rozdziela się na kilka gałęzi?",
+        odpowiedzi: ["Szeregowe", "Równoległe", "Mieszane", "Indukcyjne"],
+        poprawna: 1
+      },
+      {
+        pytanie: "Jakie jest napięcie między dwoma punktami obwodu, jeśli przeniesienie ładunku 1 kulomba wymaga pracy 1 dżula?",
+        odpowiedzi: ["1 wolt", "1 amper", "1 om", "1 wat"],
+        poprawna: 0
+      },
+      {
+        pytanie: "Które urządzenie może pełnić funkcję zarówno amperomierza, jak i woltomierza?",
+        odpowiedzi: ["Multimetr", "Transformator", "Oscyloskop", "Rezystor"],
+        poprawna: 0
+      },
+      {
+        pytanie: "Co jest niezbędne do powstania prądu elektrycznego w obwodzie?",
+        odpowiedzi: ["Opór", "Napięcie", "Moc", "Ładunek"],
+        poprawna: 1
+      },
+      {
+        pytanie: "Jak nazywa się uporządkowany ruch cząstek obdarzonych ładunkiem elektrycznym?",
+        odpowiedzi: ["Prąd elektryczny", "Napięcie elektryczne", "Opór elektryczny", "Moc elektryczna"],
+        poprawna: 0
+      },
+      {
+        pytanie: "Która z poniższych wielkości jest miarą zdolności przewodnika do przeciwstawiania się przepływowi prądu?",
+        odpowiedzi: ["Napięcie", "Natężenie", "Opór", "Moc"],
+        poprawna: 2
+      }
+  ];
--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-const pytania = [
-    {
-        pytanie: "Co jest jednostką natężenia prądu w układzie SI?",
-        odpowiedzi: ["Wolt", "Amper", "Om", "Kulomb"],
-        poprawna: 1
-      },
-      {
-        pytanie: "Jak nazywa się prawo opisujące zależność między napięciem, natężeniem prądu i oporem?",
-        odpowiedzi: ["Prawo Pascala", "Prawo Ohma", "Prawo Archimedesa", "Prawo Coulomba"],
-        poprawna: 1
-      },
-      {
-        pytanie: "Które urządzenie służy do pomiaru napięcia elektrycznego?",
-        odpowiedzi: ["Amperomierz", "Watomierz", "Woltomierz", "Ohmomierz"],
-        poprawna: 2
-      },
-      {
-        pytanie: "Jakie są nośniki prądu w metalach?",
-        odpowiedzi: ["Protony", "Elektrony", "Neutrony", "Jony dodatnie"],
-        poprawna: 1
-      },
-      {
-        pytanie: "Które z poniższych połączeń odbiorników powoduje, że prąd elektryczny rozdziela się na kilka gałęzi?",
-        odpowiedzi: ["Szeregowe", "Równoległe", "Mieszane", "Indukcyjne"],
-        poprawna: 1
-      },
-      {
-        pytanie: "Jakie jest napięcie między dwoma punktami obwodu, jeśli przeniesienie ładunku 1 kulomba wymaga pracy 1 dżula?",
-        odpowiedzi: ["1 wolt", "1 amper", "1 om", "1 wat"],
-        poprawna: 0
-      },
-      {
-        pytanie: "Które urządzenie może pełnić funkcję zarówno amperomierza, jak i woltomierza?",
-        odpowiedzi: ["Multimetr", "Transformator", "Oscyloskop", "Rezystor"],
-        poprawna: 0
-      },
-      {
-        pytanie: "Co jest niezbędne do powstania prądu elektrycznego w obwodzie?",
-        odpowiedzi: ["Opór", "Napięcie", "Moc", "Ładunek"],
-        poprawna: 1
-      },
-      {
-        pytanie: "Jak nazywa się uporządkowany ruch cząstek obdarzonych ładunkiem elektrycznym?",
-        odpowiedzi: ["Prąd elektryczny", "Napięcie elektryczne", "Opór elektryczny", "Moc elektryczna"],
-        poprawna: 0
-      },
-      {
-        pytanie: "Która z poniższych wielkości jest miarą zdolności przewodnika do przeciwstawiania się przepływowi prądu?",
-        odpowiedzi: ["Napięcie", "Natężenie", "Opór", "Moc"],
-        poprawna: 2
-      }
-  ];
-
 let los = 0;
 let a = 0;
 let byly = [];
@@ -68,26 +15,28 @@ let nastepne = document.getElementById("nastepne");
 let progi = document.getElementById("progi");
 let publicznoscOkno = document.getElementById("publicznoscOkno");
 let telefonOkno = document.getElementById("telefonOkno");
-
+let gwarantowane = document.getElementById("gwarantowane");
 
 let szczebel = document.querySelectorAll('.szczebel');
-let szczebelPogrubienie = szczebel.length -1;
-let stawkiGwarantowane = [szczebel[5].innerHTML, szczebel[10].innerHTML];
 let szczebelIndex = szczebel.length -1;
 
 let pokazPytanie = () => {
+    a++;
     los = Math.floor(Math.random() * pytania.length);
     p = pytania[los];
     pytania.splice(los,1);
 
     czyodp = false;
-    // szczebel[szczebelPogrubienie].classList.add("pogrubienie");
-
-    szczebelIndex = szczebelPogrubienie - a;
     szczebel[szczebelIndex].classList.add("pogrubienie");
+    document.getElementById("apytanie").textContent = "Pytanie "+(a)+"/12";
 
-    document.getElementById("apytanie").textContent = "Pytanie "+(a+1)+"/12";
-
+    if(szczebel[szczebelIndex].innerHTML > 40000 && szczebel[szczebelIndex].innerHTML > 1000){
+        gwarantowane.textContent = "Gwarantowane: 40 000 zł";
+    }
+    if(szczebel[szczebelIndex].innerHTML > 1000){
+        gwarantowane.textContent = "Gwarantowane: 1000 zł";
+    }
+                
     nastepne.style.display = "none";
     pytanie.textContent = p.pytanie;
 
@@ -124,7 +73,14 @@ odpowiedzi.forEach(e => {
             let podana = e.id;
             if(podana == p.poprawna){
                 e.classList.add("poprawna");
-                nastepne.style.display = "block";
+                if(a < 12){
+                  nastepne.style.display = "block";
+                }else{
+                  // document.getElementById("quiz").style.display = "none";
+                  // document.getElementById("menu").style.display = "none";
+                  document.getElementById("wygrana").style.display = "block";
+                }
+                
             }else{
                 e.classList.add("zla");
                 odpowiedzi[p.poprawna].classList.add("poprawna");
@@ -145,16 +101,9 @@ odpowiedzi.forEach(e => {
 });
 
 nastepne.addEventListener("click",() => {
-    a++;
-    szczebel[szczebelIndex].classList.remove("pogrubienie");
-    if(a < 12){
+        szczebel[szczebelIndex].classList.remove("pogrubienie");
+        szczebelIndex--;
         pokazPytanie();
-    }else{
-        nastepne.style.display = "none";
-        // document.getElementById("quiz").style.display = "none";
-        // document.getElementById("menu").style.display = "none";
-        document.getElementById("wygrana").style.display = "block";
-    }
 })
 
 let wroc = () => {
